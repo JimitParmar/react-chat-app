7fa5c72aeeebc8e4b9a79a39b833edb1fcf16878
AtharvaW29 <93931071+AtharvaW29@users.noreply.github.com>
2022-01-31 12:30:35 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ import ChatFeed from './ChatFeed';
 import LoginForm from './LoginForm';
 
 const App = () => {
-    // if(!localStorage.getItem('username')) return <LoginForm/>
+     if(!localStorage.getItem('username')) return <LoginForm/>
      return (
          <ChatEngine
            height="100vh"
            projectID="b0966623-65a3-4865-ad36-e6a61f72ee27"
-          // userName={localStorage.getItem('username')}
-           userName='Max'
-          // userSecret={localStorage.getItem('password')}
-          userSecret='98920'
+           userName={localStorage.getItem('username')}
+          // userName='Max'
+           userSecret={localStorage.getItem('password')}
+         // userSecret='98920'
            render ChatFeed = {(chatAppProps) => <ChatFeed {...chatAppProps} /> }
          />
      ); 
